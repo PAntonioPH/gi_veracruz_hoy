ed6abec852ee11aae3d2644a659f99ac06aeccfd
PAntonioPH <antonio.paph1@gmail.com>
2023-08-30 15:42:25 -0600

    Corrección de nombre de categorias home

--- src/pages/api/v1/home/index.ts
+++ src/pages/api/v1/home/index.ts
@@ -9,11 +9,23 @@ interface Category {
   father?: boolean
 }
 
-const toCamelCase = (str: string): string => str.replace(/\s(.)/g, function ($1) {
-  return $1.toUpperCase();
-}).replace(/\s/g, '').replace(/^(.)/, function ($1) {
-  return $1.toLowerCase();
-});
+const removeAccents = (str: string) => {
+  return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+}
+
+const toCamelCase = (str: string): string => {
+  // Quitar acentos antes de transformar a camel case
+  str = removeAccents(str);
+
+  return str
+    .replace(/\s(.)/g, ($1) => {
+      return $1.toUpperCase();
+    })
+    .replace(/\s/g, '')
+    .replace(/^(.)/, ($1) => {
+      return $1.toLowerCase();
+    });
+}
 
 
 const Home = async (req: NextApiRequest, res: NextApiResponse) => {
