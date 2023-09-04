332a21b5bebdd34776d667cbab11fcab8048184b
pamel <pamela.burgosesp@gmail.com>
2023-09-04 15:21:57 -0600

    eliminacion de campañas y espacios

--- src/components/Layout/AnnouncementsRight.tsx
+++ src/components/Layout/AnnouncementsRight.tsx
@@ -1,14 +1,9 @@
-import {Box, Divider, Flex, Heading, Image, useBreakpointValue} from "@chakra-ui/react";
+import {Box, Divider, Flex, Heading, useBreakpointValue} from "@chakra-ui/react";
 import {TimelineTwitter} from "@/components/SocialMedia/TimelineTwitter";
 import {TimelineFacebook} from "@/components/SocialMedia/TimelineFacebook";
 import React from "react";
-import {LastNews} from "@/components/LastNews";
-import {useRouter} from "next/router";
-import {VideoFB} from "@/components/VideoFB";
 
 export const AnnouncementsRight = () => {
-  const router = useRouter()
-  const isDesktop = useBreakpointValue({base: false, xl: true})
   return (<Flex
       as="aside"
       direction="column"
@@ -24,116 +19,15 @@ export const AnnouncementsRight = () => {
         <Divider borderColor="#a02142" borderWidth="1px"/>
       </Box>
 
-      <Flex
-        justifyContent="center"
-        alignItems="center"
-        direction="column"
-      >
-        <Heading
-          as="h1"
-          size="lg"
-          textAlign="center"
-          mb={5}
-          textTransform="uppercase"
-          textDecoration={"underline"}
-        >
-          El día en un minuto
-        </Heading>
-
-        <VideoFB
-          id={1}
-        />
-      </Flex>
-
-      <Divider my={5} borderColor="gray.200" borderWidth="1px"/>
-
-      <LastNews/>
-
-      <Image
-        src="/assets/icons/boton.png"
-        alt="boton"
-        onClick={() => window.open(`mailto:laopiniondelaltiplano@gmail.com?subject=Quiero%20hacer%20un%20Reporte`, '_blank')}
-        cursor={"pointer"}
-      />
-
-      <Divider my={5} borderColor="gray.200" borderWidth="1px"/>
-
-      <Image
-        src="/assets/icons/periodico.png"
-        alt="periodico"
-        onClick={() => router.push("/news")}
-        cursor={"pointer"}
-      />
-
-      <Divider my={5} borderColor="gray.200" borderWidth="1px"/>
-
       <Divider my={5} borderColor="gray.200" borderWidth="1px"/>
       <TimelineFacebook/>
 
-
-      <Divider my={5} borderColor="gray.200" borderWidth="1px"/>
-      <Box>
-        <Image
-          src={`/assets/campanas/padre.jpg`}
-          alt={'Casa de empeño'}
-        />
-      </Box>
       <Divider my={5} borderColor="gray.200" borderWidth="1px"/>
 
       <TimelineTwitter/>
 
-
       <Divider my={5} borderColor="gray.200" borderWidth="1px"/>
 
-
-      {/*<Box>*/}
-      {/*  <iframe width="250" height="300" src="https://www.youtube.com/embed/eW-A76Rkj_4"*/}
-      {/*          title="YouTube video player"*/}
-      {/*          frameBorder="0"*/}
-      {/*          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"*/}
-      {/*          allowFullScreen>*/}
-
-      {/*  </iframe>*/}
-      {/*</Box>*/}
-
-      {/*<Divider my={5} borderColor="gray.200" borderWidth="1px"/>*/}
-
-      {/*<Box>*/}
-      {/*  <iframe width="250" height="300" src="https://www.youtube.com/embed/7BSBMkU86XQ"*/}
-      {/*          title="YouTube video player"*/}
-      {/*          frameBorder="0"*/}
-      {/*          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"*/}
-      {/*          allowFullScreen>*/}
-
-      {/*  </iframe>*/}
-      {/*</Box>*/}
-
-      <Divider my={5} borderColor="gray.200" borderWidth="1px"/>
-      <Box>
-        <Image
-          src={`/assets/campanas/jm.jpg`}
-          alt={'Casa de empeño'}
-        />
-      </Box>
-
-      <Divider my={5} borderColor="gray.200" borderWidth="1px"/>
-
-      <Box>
-        <iframe width="250" height="300" src="https://www.youtube.com/embed/NW4nIEekQxc"
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen>
-
-        </iframe>
-      </Box>
-      <Divider my={5} borderColor="gray.200" borderWidth="1px"/>
-      <Box>
-        <Image
-          src={`/assets/campanas/school.jpeg`}
-          alt={'Conducir'}
-        />
-      </Box>
     </Flex>
   )
 }
