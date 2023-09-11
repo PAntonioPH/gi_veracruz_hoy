54065a3125163fffe25233cb67d931f8c7da7791
PAntonioPH <antonio.paph1@gmail.com>
2023-09-11 15:37:13 -0600

    Corrección de portada

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,60 +1,62 @@
-import {Box, Flex, HStack, IconButton, SimpleGrid, Image, useBreakpointValue, useDisclosure, Center} from '@chakra-ui/react'
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import {faBars} from "@fortawesome/free-solid-svg-icons"
-import {useRouter} from "next/router";
-import axios from "axios";
-import {NavbarMobile} from "@/components/Layout/NavbarMobile";
-import React, {useEffect, useState} from "react";
-import {Category} from "@/interfaces/Category";
-import {NavbarItem} from "@/components/Layout/NavbarItem";
-import Bubble from "@/components/Bubble";
+import { Box, Flex, HStack, IconButton, SimpleGrid, Image, useBreakpointValue, useDisclosure, Center } from '@chakra-ui/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+import { NavbarMobile } from '@/components/Layout/NavbarMobile';
+import React, { useEffect, useState } from 'react';
+import { Category } from '@/interfaces/Category';
+import { NavbarItem } from '@/components/Layout/NavbarItem';
+import Bubble from '@/components/Bubble';
 
 export const Navbar = () => {
-  const router = useRouter()
-  const isDesktop = useBreakpointValue({base: false, lg: true})
-  const {isOpen, onOpen, onClose} = useDisclosure()
-  const [pages, setPages] = useState<Category[]>([])
+  const router = useRouter();
+  const isDesktop = useBreakpointValue({ base: false, lg: true });
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [pages, setPages] = useState<Category[]>([]);
 
-  const handleClickNav = async (url: string) => await router.push(url === "/" ? "/" : `/category${url}`)
+  const handleClickNav = async (url: string) => await router.push(url === '/' ? '/' : `/category${url}`);
 
   useEffect(() => {
-    axios.get('/api/v1/categories', {
-      params: {navbar: true,},
-      headers: {Authorization: `${process.env.NEXT_PUBLIC_TOKEN_WEB}`}
-    })
-      .then(res => {
-        if (res.data.response.length > 0) setPages(res.data.response)
+    axios
+      .get('/api/v1/categories', {
+        params: { navbar: true },
+        headers: { Authorization: `${process.env.NEXT_PUBLIC_TOKEN_WEB}` },
       })
-  }, [])
+      .then((res) => {
+        if (res.data.response.length > 0) setPages(res.data.response);
+      });
+  }, []);
 
   return (
     <>
-      <Box as="section">
-        <Box as="nav" bg="bg-surface" boxShadow="sm">
-          <Center>
-            <Image
-              src="/assets/icons/portada.jpg"
-              alt="Portada"
-              objectFit="cover"
-              w={"full"}
-              h={"200px"}
-              onClick={()=>router.push('/')}
-              cursor={"pointer"}
-            />
-          </Center>
+      <Box as='section'>
+        <Box
+          as='nav'
+          bg='bg-surface'
+          boxShadow='sm'
+        >
+          <Image
+            src='/assets/icons/portada.jpg'
+            alt='Portada'
+            w={'full'}
+            h={'200px'}
+            onClick={() => router.push('/')}
+            cursor={'pointer'}
+          />
           <HStack
-            spacing="10"
-            justify="space-between"
-            px={{base: "5", lg: "10"}}
-            py={{base: "5", lg: "5"}}
-            bg={"#19232d"}
-            color={"white"}
+            spacing='10'
+            justify='space-between'
+            px={{ base: '5', lg: '10' }}
+            py={{ base: '5', lg: '5' }}
+            bg={'#19232d'}
+            color={'white'}
           >
-            {isDesktop
-              ? (<Flex
-                justifyContent={"center"}
-                flex="1"
-                alignItems="center"
+            {isDesktop ? (
+              <Flex
+                justifyContent={'center'}
+                flex='1'
+                alignItems='center'
               >
                 <SimpleGrid
                   columns={pages.length}
@@ -68,15 +70,16 @@ export const Navbar = () => {
                     />
                   ))}
                 </SimpleGrid>
-              </Flex>)
-              : (<IconButton
-                variant="ghost"
-                icon={<FontAwesomeIcon icon={faBars}/>}
-                aria-label="Open Menu"
+              </Flex>
+            ) : (
+              <IconButton
+                variant='ghost'
+                icon={<FontAwesomeIcon icon={faBars} />}
+                aria-label='Open Menu'
                 onClick={onOpen}
-              />)
-            }
-            <Bubble/>
+              />
+            )}
+            <Bubble />
           </HStack>
         </Box>
       </Box>
@@ -87,5 +90,5 @@ export const Navbar = () => {
         pages={pages}
       />
     </>
-  )
-}
+  );
+};
