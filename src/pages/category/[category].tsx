104be830ae0a56495e78c95a24307bd908b14c56
pamel <pamela.burgosesp@gmail.com>
2023-09-13 11:28:44 -0600

    corrección de BreadcrumbsItem en margin y center

--- src/pages/category/[category].tsx
+++ src/pages/category/[category].tsx
@@ -7,7 +7,7 @@ import {Post} from "@/interfaces/Post";
 import {LoadingPage} from "@/components/LoadingPage";
 import {Pagination} from "@/components/Pagination";
 import moment from "moment";
-import {Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator,} from '@chakra-ui/react'
+import {Breadcrumb, BreadcrumbItem, BreadcrumbLink} from '@chakra-ui/react'
 
 
 const Category = () => {
@@ -62,7 +62,7 @@ const Category = () => {
                     category
                   }
                 </Heading>
-              </Box>
+              </Box> 
                 <Breadcrumb
                   separator={'>'}
                   color={"#aaaabd"}
@@ -174,60 +174,6 @@ const Category = () => {
                         Leer más
                       </Button>
                     </Center>
-
-                    {/*<Flex*/}
-                    {/*  direction={"column"}*/}
-                    {/*  alignItems={"center"}*/}
-                    {/*>*/}
-                    {/*  <Heading*/}
-                    {/*    size={"md"}*/}
-                    {/*    pb={2}*/}
-                    {/*    mt={2}*/}
-                    {/*    mx={2}*/}
-                    {/*  >*/}
-                    {/*    {*/}
-                    {/*      title.slice(0, 70)*/}
-                    {/*    }*/}
-                    {/*    {*/}
-                    {/*      title.length > 70*/}
-                    {/*      && "..."*/}
-                    {/*    }*/}
-                    {/*  </Heading>*/}
-                    {/*  <Text*/}
-                    {/*    pb={5}*/}
-                    {/*    as={"em"}*/}
-                    {/*  >*/}
-                    {/*    {moment(date_update.split("T")[0]).format("DD/MM/YYYY")}*/}
-                    {/*  </Text>*/}
-                    {/*  <Image*/}
-                    {/*    objectFit='cover'*/}
-                    {/*    h={"300px"}*/}
-                    {/*    w={"300px"}*/}
-                    {/*    src={img && img != "" ? img : '/assets/images/placeholderImg.jpg'}*/}
-                    {/*    alt='Imagen'*/}
-                    {/*  />*/}
-                    {/*  <Text*/}
-                    {/*    px={5}*/}
-                    {/*    py={5}*/}
-                    {/*    textAlign={"justify"}*/}
-                    {/*  >*/}
-                    {/*    {*/}
-                    {/*      content.slice(0, 130)*/}
-                    {/*    }*/}
-                    {/*    {*/}
-                    {/*      content.length > 131*/}
-                    {/*      && "..."*/}
-                    {/*    }*/}
-                    {/*  </Text>*/}
-                    {/*  <Button*/}
-                    {/*    variant={"outline"}*/}
-                    {/*    colorScheme={"gray"}*/}
-                    {/*    mb={5}*/}
-                    {/*  >*/}
-                    {/*    Leer más*/}
-                    {/*  </Button>*/}
-                    {/*</Flex>*/}
-
                   </Flex>
                 ))
               }
