50bfaf09c622a18a17d93b4a40100ae875726404
PAntonioPH <antonio.paph1@gmail.com>
2023-09-14 12:19:39 -0600

    Corrección de categoría vacía

--- src/components/Home/Category.tsx
+++ src/components/Home/Category.tsx
@@ -23,86 +23,91 @@ interface Props {
 export const Category = ({posts, name, timeline}: Props) => {
   const isDesktop = useBreakpointValue({base: false, md: true})
 
-  return (<Box
-  >
-    <Box
-      bg={"#333946"}
-      w={"full"}
-      mt={"20"}
-    >
-      <Heading
-        p={2}
-        mb={5}
-        w={"fit-content"}
-        bg={"#d13030"}
-        size={"sm"}
-        color={"white"}
+  return (<>
+    {
+      posts && posts.length > 0
+      && (<Box
       >
-        {
-          name
-        }
-      </Heading>
-    </Box>
-    <Stack
-      direction={{md: "row", base: "column"}}
-      w={"100%"}
-      spacing={10}
-      h={{md: timeline ? "1000px" : "500px", base: "auto"}}
-    >
-      {
-        (!isDesktop && !timeline) && (<LastPostCategory post={posts[0]}/>)
-      }
-
-      <Stack
-        w={{md: timeline ? "35%": "25%", base: "100%"}}
-        spacing={5}
-        p={2}
-      >
-        {
-          timeline
-            ? (<>
-              <PostCategory post={posts[0]}/>
-              <PostCategory post={posts[1]}/>
-            </>)
-            : (<>
-              <PostCategory post={posts[1]}/>
-              <PostCategory post={posts[2]}/>
-            </>)
-        }
-      </Stack>
-
-      {
-        (isDesktop && !timeline) && (<LastPostCategory post={posts[0]}/>)
-      }
-
-      <Stack
-        w={{md: timeline ? "35%": "25%", base: "100%"}}
-        spacing={5}
-        p={2}
-      >
-        {
-          timeline
-            ? (<>
-              <PostCategory post={posts[2]}/>
-              <PostCategory post={posts[3]}/>
-            </>)
-            : (<>
-              <PostCategory post={posts[3]}/>
-              <PostCategory post={posts[4]}/>
-            </>)
-        }
-      </Stack>
-
-      {
-        timeline
-        && (<Box
-          w={{md: "30%", base: "100%"}}
+        <Box
+          bg={"#333946"}
+          w={"full"}
+          mt={"20"}
         >
-          <TimelineFacebook/>
-          <Divider my={5}/>
-          <TimelineTwitter/>
-        </Box>)
-      }
-    </Stack>
-  </Box>)
+          <Heading
+            p={2}
+            mb={5}
+            w={"fit-content"}
+            bg={"#d13030"}
+            size={"sm"}
+            color={"white"}
+          >
+            {
+              name
+            }
+          </Heading>
+        </Box>
+        <Stack
+          direction={{md: "row", base: "column"}}
+          w={"100%"}
+          spacing={10}
+          h={{md: timeline ? "1000px" : "500px", base: "auto"}}
+        >
+          {
+            (!isDesktop && !timeline) && (<LastPostCategory post={posts[0]}/>)
+          }
+
+          <Stack
+            w={{md: timeline ? "35%" : "25%", base: "100%"}}
+            spacing={5}
+            p={2}
+          >
+            {
+              timeline
+                ? (<>
+                  <PostCategory post={posts[0]}/>
+                  <PostCategory post={posts[1]}/>
+                </>)
+                : (<>
+                  <PostCategory post={posts[1]}/>
+                  <PostCategory post={posts[2]}/>
+                </>)
+            }
+          </Stack>
+
+          {
+            (isDesktop && !timeline) && (<LastPostCategory post={posts[0]}/>)
+          }
+
+          <Stack
+            w={{md: timeline ? "35%" : "25%", base: "100%"}}
+            spacing={5}
+            p={2}
+          >
+            {
+              timeline
+                ? (<>
+                  <PostCategory post={posts[2]}/>
+                  <PostCategory post={posts[3]}/>
+                </>)
+                : (<>
+                  <PostCategory post={posts[3]}/>
+                  <PostCategory post={posts[4]}/>
+                </>)
+            }
+          </Stack>
+
+          {
+            timeline
+            && (<Box
+              w={{md: "30%", base: "100%"}}
+            >
+              <TimelineFacebook/>
+              <Divider my={5}/>
+              <TimelineTwitter/>
+            </Box>)
+          }
+        </Stack>
+      </Box>)
+    }
+  </>)
 }
